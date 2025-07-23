6a5212ab553dd0ca8466fbe42fd91dc7673adeac
PhenChua <89496400+PhenChua29@users.noreply.github.com>
2025-07-24 02:32:56 +0700

    Improve bot's logic

--- src/object/Move.java
+++ src/object/Move.java
@@ -4,11 +4,19 @@ package object;
  * A move on the board.
  */
 public class Move {
-  private int row, col;
+
+  private int row, col, score;
 
   public Move(int row, int col) {
     this.row = row;
     this.col = col;
+    score = 0;
+  }
+
+  public Move(int row, int col, int score) {
+    this.row = row;
+    this.col = col;
+    this.score = score;
   }
 
   public int getRow() {
@@ -18,4 +26,9 @@ public class Move {
   public int getCol() {
     return col;
   }
+
+  public int getScore() {
+    return score;
+  }
+
 }
